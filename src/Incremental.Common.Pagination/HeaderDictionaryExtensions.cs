b725ad4d5114db27b97ad89e7799c119d4193566
Lucas Maximiliano Marino <hello@lucasmarino.me>
2022-01-14 19:36:32 +0200

    feat: improve Header extensions api

--- src/Incremental.Common.Pagination/HeaderDictionaryExtensions.cs
+++ src/Incremental.Common.Pagination/HeaderDictionaryExtensions.cs
@@ -11,25 +11,28 @@ namespace Incremental.Common.Pagination
     public static class HeaderDictionaryExtensions
     {
         /// <summary>
-        /// Adds pagination metadata to headers in response.
+        /// Adds pagination metadata to the headers dictionary.
         /// Default key for pagination metadata: `X-Pagination`
         /// </summary>
         /// <param name="header">The <see cref="IHeaderDictionary"/> that will be used.</param>
         /// <param name="data">The paginated list data to add to the header.</param>
+        /// <param name="key">Key to use in header</param>
         /// <typeparam name="T">The data.</typeparam>
-        public static void AddPagination<T>(this IHeaderDictionary header, PaginatedList<T> data)
+        public static void AddPagination<T>(this IHeaderDictionary header, PaginatedList<T> data, string key = "X-Pagination")
         {
-            header.Add("X-Pagination", JsonSerializer.Serialize(data.ExtractMetadata()));
+            header.Add(key, JsonSerializer.Serialize(data.ExtractMetadata()));
         }
 
         /// <summary>
         /// Extracts pagination information if available.
+        /// Default key for pagination metadata: `X-Pagination`
         /// </summary>
         /// <param name="headers">The <see cref="HttpResponseHeaders"/> where the pagination information may be.</param>
+        /// <param name="key">Key to use in header</param>
         /// <returns>Pagination information as <see cref="XPaginationMetadata"/></returns>
-        public static XPaginationMetadata? GetPagination(this HttpResponseHeaders headers)
+        public static XPaginationMetadata? GetPagination(this HttpResponseHeaders headers, string key = "X-Pagination")
         {
-            var pagination = headers.FirstOrDefault(pair => pair.Key == "X-Pagination").Value.FirstOrDefault();
+            var pagination = headers.FirstOrDefault(pair => pair.Key == key).Value.FirstOrDefault();
 
             return pagination is not null ? JsonSerializer.Deserialize<XPaginationMetadata>(pagination) : default;
         }
